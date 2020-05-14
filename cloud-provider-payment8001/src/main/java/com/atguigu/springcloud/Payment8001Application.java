6bdb0137b37e107934e183405c2987cd63fc7383
luwei0o0 <771608610@qq.com>
2020-05-14 23:58:51 +0800

    业务相关代码

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/Payment8001Application.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/Payment8001Application.java
@@ -1,11 +1,12 @@
 package com.atguigu.springcloud;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Payment8001Application {
-    
+
     public static void main(String[] args){
         SpringApplication.run(Payment8001Application.class,args);
     }
