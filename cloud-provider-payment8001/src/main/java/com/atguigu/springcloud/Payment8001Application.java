08983807ef8146522fbbca4faf6a2bd9bd582f3b
luwei <luwei09@sinochem.com>
2020-05-15 13:49:54 +0800

    支付微服务8001入驻进eurekaserver

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/Payment8001Application.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/Payment8001Application.java
@@ -1,10 +1,11 @@
 package com.atguigu.springcloud;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Payment8001Application {
 
     public static void main(String[] args){
